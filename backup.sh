84688fd153c7b6142e315ddb8338758dca697c63
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2021-05-17 10:02:54 +0200

    fix typos & reworked crontab

--- backup.sh
+++ backup.sh
@@ -17,19 +17,21 @@ fi
 
 echo -e "Please enter the source backup folder, which get archived for example '/home/backup'\n"
 
-read SRCDIR
+read -r SRCDIR
+echo ""
 
 while :; do
 if [ -d "$SRCDIR" ]; then
 	break
 else 
-	if [ "$SRCDIR" == "l" ] || [ "$SRCDIR" == "exit" ] || [ "$SRCDIR" == "q" || [ "$SRCDIR" == "leave" || [ "$SRCDIR" == "quit" ]; then
+	if [ "$SRCDIR" == "l" ] || [ "$SRCDIR" == "exit" ] || [ "$SRCDIR" == "q" ] || [ "$SRCDIR" == "leave" ] || [ "$SRCDIR" == "quit" ]; then
 		echo "You have cancelled the script. So this script will exit now."
 		exit 1
 	else
 		echo "Error: Not a valid path"
 		echo -e "Please enter the source backup folder, which get archived for example '/home/backup'\n"
-		read SRCDIR
+		read -r SRCDIR
+		echo ""
 	fi
 fi
 done
@@ -40,24 +42,52 @@ done
 
 echo -e "Please enter the destination backup folder, which get archived for example '/home/backup'\n"
 
-read DESDIR
+read -r DESDIR
+echo ""
 
 while :; do
-if ! [ $DESDIR = $SRCDIR ]; then
+if ! [ "$DESDIR" = "$SRCDIR" ]; then
 	if [ -d "$DESDIR" ]; then
 		break
 	else 
 		echo "Error: Not a valid path"
 		echo -e "Please enter the destination backup folder, which get archived for example '/home/backup'\n"
-		read DESDIR
+		read -r DESDIR
+		echo ""
 	fi
-elif [ "$DESDIR" == "l" ] || [ "$DESDIR" == "exit" ] || [ "$DESDIR" == "q" ] || [ "$DESDIR" == "leave" || [ "$DESDIR" == "quit" ]; then
+elif [ "$DESDIR" == "l" ] || [ "$DESDIR" == "exit" ] || [ "$DESDIR" == "q" ] || [ "$DESDIR" == "leave" ] || [ "$DESDIR" == "quit" ]; then
 		echo "You have cancelled the script. So this script will exit now."
 		exit 1
 else
-	echo "Error: You have the same path"
-	echo -e "Enter source backup folder\n"
-	read DESDIR
+	echo -e "Error: You have the same path"
+	echo -e "Please enter the destination backup folder, which get archived for example '/home/backup'\n"
+	read -r DESDIR
+	echo ""
+fi
+done
+
+#----------------------------------------------------------
+# Filename
+#----------------------------------------------------------
+
+read -r -p "Please enter the desired name of the backup that comes before the time: " FPART
+
+echo ""
+
+VALIDATE='[^a-zA-Z]'
+
+while :; do
+if [[ $FPART =~ $VALIDATE ]]; then
+	if [ "$FPART" == "l" ] || [ "$FPART" == "exit" ] || [ "$FPART" == "q" ] || [ "$FPART" == "leave" ] || [ "$FPART" == "quit" ]; then
+			echo "You have cancelled the script. The script will exit now."
+			exit 1
+	else
+		echo "Error: Not a valid filename"
+		read -r -p "Please enter the name of the backup that comes before the time: " FPART
+		echo ""
+	fi
+else
+	break
 fi
 done
 
@@ -69,56 +99,39 @@ echo -e "Do you want that the script will be executed automatically?\n"
 
 read -r -p "Please insert 'YES' if you want it, otherwise it would be skipped! " ANSWER
 
-if [ "$ANSWER" == "YES" ] || [ "$ANSWER" == "y" || [ "\$ANSWER" == "Y" || [ "\$ANSWER" == "yes" ]; then
+echo ""
+
+if [ "$ANSWER" == "YES" ] || [ "$ANSWER" == "y" ] || [ "$ANSWER" == "Y" ] || [ "$ANSWER" == "yes" ]; then
 
 echo -e "Enter time in hour (0 - 23) when the backup should be daily generated\n"
 
-read CRONTAB
+read -r CRONTAB
 
 re='^[0-9]+$'
 
 while :; do
 if ! [[ $CRONTAB =~ $re ]] ; then
-		if [ "$CRONTAB" == "l" ] || [ "$CRONTAB" == "exit" ] || [ "$CRONTAB" == "q" ] || [ "$CRONTAB" == "leave" || [ "$CRONTAB" == "quit" ]; then
+		if [ "$CRONTAB" == "l" ] || [ "$CRONTAB" == "exit" ] || [ "$CRONTAB" == "q" ] || [ "$CRONTAB" == "leave" ] || [ "$CRONTAB" == "quit" ]; then
 			echo "You have cancelled the script. So this script will exit now."
 			exit 1
 		else
 			echo "Error: Not a number"
-			echo -e "Enter time"
-			read CRONTAB
+			echo -e "Enter time in hour (0 - 23) when the backup should be daily generated\n"
+			read -r CRONTAB
+			echo ""
 		fi
 else
-	break
-fi
-done
-
-crontab -r backupscript
-crontab -e backupscript | { cat; echo echo "00 $CRONTAB * * * /bin/bash tar -cpzf $DESDIR/$FILENAME -P $SRCDIR >/dev/null 2>&1"; } | crontab -
-
-else
-	echo "Your answer was \"$ANSWER\" and not YES. Therefore this step will be skipped...."
-fi
-
-#----------------------------------------------------------
-# Filename
-#----------------------------------------------------------
-
-read -r -p "Please enter the desired name of the backup that comes before the time: " FPART
-
-VALIDATE='[^a-zA-Z]'
-
-while :; do
-if [[ $FPART =~ $VALIDATE ]]; then
-	if [ "$FPART" == "l" ] || [ "$FPART" == "exit" ] || [ "$FPART" == "q" || [ "$FPART" == "leave" || [ "$FPART" == "quit" ]; then
-			echo "You have cancelled the script. The script will exit now."
-			exit 1
-	else
-		echo "Error: Not a valid filename"
-		read -r -p "Please enter the name of the backup that comes before the time: " FPART
-	fi
-else
+	( crontab -l | grep -v -F 'tar' ; echo "00 0$CRONTAB * * * /bin/bash tar -cpzf $DESDIR/$FPART-date +%d-%m-%y-%H-%M.tar.gz -P $SRCDIR" ) | crontab -
 	echo -e "Setup done!"
 	echo -e "The backup process was completed successfully."
 	break
 fi
 done
+
+else
+	echo "Your answer was \"$ANSWER\" and not YES. Therefore this step will be skipped...."
+	echo -e "Setup done!"
+	echo -e "The backup process was completed successfully."
+fi
+
+
